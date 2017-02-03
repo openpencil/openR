e4bf7c84b349c818d80dcf2ffc71e840e2507e36
openpencil <jyoti.shankar@icloud.com>
2017-02-03 01:02:40 -0500

    welch eg for replicate() and the bootstrap CI procedure

--- openr_vocabulary.R
+++ openr_vocabulary.R
@@ -79,3 +79,76 @@ sink("file_that_will_store_stuff.txt")
 summary(glm(case ~ spontaneous + induced,data = infert, family = binomial()))
 #' Close the sink
 sink()
+
+#***************************************************************#
+# All other functions in the vocabulary chapter that I explored
+# a bit more
+# **************************************************************#
+
+# Files and directories
+# dir() is just list.dirs() with full.names and recursive
+# arguments set to false
+# basename, dirname, tools::file_ext
+# file.path
+# path.expand, normalizePath
+# file.choose
+# file.copy, file.create, file.remove, file.rename, dir.create
+# file.exists, file.info
+# tempdir, tempfile
+# download.file, library(downloader)
+
+### How to use replicate to generate bootstrap confidence intervals of difference between 2 groups
+# generate some data
+group1 <- log2(round(rnorm(n = 100, mean = 500, sd = 50), 0))
+group2 <- log2(round(rnorm(n = 900, mean = 525, sd = 40), 0))
+
+# visualize the distribution of datasets
+# make a dataframe
+ggdata <- data.frame(Counts = c(group1, group2), stringsAsFactors = F)
+# Add a column with the group label
+ggdata$group <- c(rep(x = "group1", times = length(group1)),
+                  rep(x = "group2", times = length(group2)))
+library(ggplot2)
+# Call ggplot, define the dataframe for plotting, define the x and y axis
+p <- ggplot(data = ggdata, mapping = aes(x = group, y = Counts))
+p <- p + geom_violin()
+p <- p + xlab("")
+p
+# Visualized another way
+p <- ggplot(data = ggdata, mapping = aes_string(x = "Counts", y = "..density.."))
+p <- p + geom_density(aes(fill = group), alpha = 0.4)
+# Draw the means
+p <- p + geom_vline(xintercept = c(mean(group1), mean(group2)), linetype = "dashed",colour = "grey30")
+p <- p + xlab("Value") + ylab("Density")
+p
+
+# Calculate the bootstrap confidence interval of differences in means
+#
+bootfunction <- function(){
+  # set.seed(round(runif(n = 1, min = 9, max = 788), 0))
+  # sample by the size of the group
+  group1data <- ggdata[which(ggdata$group == "group1"),]
+  group2data <- ggdata[which(ggdata$group == "group2"),]
+  samplerows1 <- sample(x=(1:nrow(group1data)), size=nrow(group1data), replace = TRUE)
+  samplerows2 <- sample(x=(1:nrow(group2data)), size=nrow(group2data), replace = TRUE)
+  group1vals <- group1data[samplerows1, "Counts"]
+  group2vals <- group2data[samplerows2, "Counts"]
+  # welch statistic
+  if (length(group1vals) == 0  | length(group2vals) == 0){
+    return(NA)
+  } else {
+    wnum <- mean(group1vals) - mean(group2vals)
+    wdenom <- sqrt((sd(group1vals))^2/length(group1vals) +
+                     (sd(group2vals))^2/length(group2vals))
+    wtest <- wnum/wdenom
+    return(wtest)
+  }
+}
+# repeat the function 5000 times
+bootdo <- na.exclude(replicate(100, bootfunction()))
+#  bootdo <- bootdo[-which(is.infinite(bootdo))]
+bootci <- quantile(bootdo, prob=c(0.025, 0.975))
+bootdf <- data.frame(lowerci=bootci["2.5%"], upperci=bootci["97.5%"])
+
+
+
